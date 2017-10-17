16d195e99c5500368e118560dc5699c8c5b5fe7a
Fabrice MILA <f.mila@etechconsulting-mg.com>
2017-10-17 15:58:48 +0300

    an other fix directories

---
+++ src/app/contrainte/config/_app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from '../../../presentation/_app/app.component';
+
+// module presentation
+import { ContactModule } from '../../../presentation/contact/contact.module';
+import { HomeModule } from '../../../presentation/home/home.module';
+
+// module service applicatif
+import { ServiceApplicatifModule } from '../../../service-applicatif/service-applicatif.module';
+
+// module service metier
+import { ServiceMetierModule } from '../../../service-metier/service-metier.module';
+
+// module service business delegate
+import { ServiceBusinessDelegateModule } from '../../../service-business-delegate/service-business-delegate.module';
+
+// module routing
+import { AppRoutingModule } from '../../../contrainte/config/app-routing.module';
+
+// component flash message
+import { FlashMessageComponent } from '../../../presentation/flash-message/flash-message.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FlashMessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ContactModule,
+    HomeModule,
+    ServiceApplicatifModule,
+    AppRoutingModule,
+    ServiceBusinessDelegateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
