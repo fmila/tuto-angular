8d9e852f03bb3512a0b12d3c7fcec9e61e0aefd1
Fabrice MILA <f.mila@etechconsulting-mg.com>
2017-10-19 15:13:03 +0300

    fix unit test contact

--- src/app/presentation/contact/contact.component.spec.ts
+++ src/app/presentation/contact/contact.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ContactComponent } from './contact.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { ContactCudApplicatifMockService, ContactCudApplicatifServiceACI, ContactReadApplicatifMockService, ContactReadApplicatifServiceACI } from '../../service-applicatif/contact/';
+import { FlashMessageService } from '../../presentation/flash-message/flash-message.service';
+
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,7 +14,13 @@ describe('ContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ ContactComponent, ContactListComponent, ContactDetailComponent ],
+      providers: [
+        { provide: ContactReadApplicatifServiceACI, useClass: ContactReadApplicatifMockService },
+        { provide: ContactCudApplicatifServiceACI, useClass: ContactCudApplicatifMockService },
+        FlashMessageService
+      ]
     })
     .compileComponents();
   }));
