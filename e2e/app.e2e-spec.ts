6fdde91367a99903de036fd0b29b334ebc7a6f05
Fabrice MILA <f.mila@etechconsulting-mg.com>
2017-10-25 16:13:45 +0300

    run test end-to-end

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -6,10 +6,9 @@ describe('tuto-contact-angular App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-/*
+
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Hello, welcome!');
   });
-*/
 });
