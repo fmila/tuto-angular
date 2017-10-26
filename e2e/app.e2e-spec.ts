a5646d4a53471fd24848e9d1bc63f82c770c4477
Fabrice MILA <f.mila@etechconsulting-mg.com>
2017-10-26 10:37:18 +0300

    e2e click event

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('tuto-contact-angular App', () => {
   let page: AppPage;
@@ -12,9 +13,16 @@ describe('tuto-contact-angular App', () => {
     expect(page.getParagraphText()).toEqual('Hello, welcome!');
   });
 
-  it('should display contact list', () => {
+  it('should display contact list and can navigate to edit page', () => {
     page.navigateTo('/contact/list');
     let contacts = page.countContactList();
-    expect(contacts.count()).toEqual(4);
-  })
+    expect(contacts.count()).toBeGreaterThan(1);
+    let tr = element.all(by.css("app-contact-list .table-striped tbody tr")).first();
+    let td = tr.all(by.css("td a")).first();
+    td.click().then(v => {
+        browser.waitForAngular();
+        expect(browser.driver.getCurrentUrl()).toMatch('/contact/edit');
+      }
+    );
+  });
 });
